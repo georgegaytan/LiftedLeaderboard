1cd48764a058dc1ef9b3f8e29d133866904ed4bf
georgegaytan <george@tatari.tv>
2025-10-12 21:30:55 +0200

    Add TODO for final release

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from src.database import start_db
 from src.services.db_manager import DBManager
 from src.utils.logs import setup_logging
 
+# TODO POC: Setup git releases, issues/branches for features,
+#  and deploy the latest main on Prod Bot on a service
 if __name__ == '__main__':
     setup_logging(logging.INFO)
 
