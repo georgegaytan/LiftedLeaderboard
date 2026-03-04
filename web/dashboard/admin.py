add4a83d9b99f1e6341982fee74ee0d3208fd181
georgegaytan <george@tatari.tv>
2026-03-04 21:59:09 +0100

    fix: explicitely hide password hashes from web admin interface (#30)

--- web/dashboard/admin.py
+++ web/dashboard/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Activity, ActivityRecord, LevelThreshold, User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('password',)
+    readonly_fields = ('last_login',)
+    list_display = ('display_name', 'email', 'level', 'total_xp', 'is_admin')
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Activity)
 admin.site.register(ActivityRecord)
 admin.site.register(LevelThreshold)
